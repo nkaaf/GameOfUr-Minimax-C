217144668f277d30f101a0a21a5396c1bcdbecaf
Niklas Kaaf <niklas.kaaf@stud.hn.de>
2025-01-09 19:07:26 +0100

    better implementation of any_piece_on_field

--- src/common.c
+++ src/common.c
@@ -1,5 +1,7 @@
 #include "common.h"
 
+#include <config.h>
+
 bool any_piece_on_field(const uint32_t pieces, const uint8_t pos, size_t* piece_index)
 {
     bool on_field = false;
@@ -10,46 +12,14 @@ bool any_piece_on_field(const uint32_t pieces, const uint8_t pos, size_t* piece_
         piece_index = &tmp;
     }
 
-    PIECE_FIELD_GET(piece_field, pieces, 0);
-    if (piece_field == pos)
+    for (short i = 0; i < NUM_OF_PIECES_PER_PLAYER; i++)
     {
-        on_field = true;
-        *piece_index = 0;
-    }
-    else
-    {
-        PIECE_FIELD_GET(piece_field, pieces, 1);
+        PIECE_FIELD_GET(piece_field, pieces, i);
         if (piece_field == pos)
         {
             on_field = true;
-            *piece_index = 1;
-        }
-        else
-        {
-            PIECE_FIELD_GET(piece_field, pieces, 2);
-            if (piece_field == pos)
-            {
-                on_field = true;
-                *piece_index = 2;
-            }
-            else
-            {
-                PIECE_FIELD_GET(piece_field, pieces, 3);
-                if (piece_field == pos)
-                {
-                    on_field = true;
-                    *piece_index = 3;
-                }
-                else
-                {
-                    PIECE_FIELD_GET(piece_field, pieces, 4);
-                    if (piece_field == pos)
-                    {
-                        on_field = true;
-                        *piece_index = 4;
-                    }
-                }
-            }
+            *piece_index = i;
+            break;
         }
     }
 
